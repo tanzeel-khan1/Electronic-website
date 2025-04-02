1bd66c463d6a7dc5d13736e634142a11b1ee338a
SYED SHABI UL HASSAN <tanzeel0680@gmail.com>
2025-04-02 19:10:58 +0500

    phir change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import React from "react";
 import "./App.css";
 
 function App() {
+    const [newWindow, setNewWindow] = useState(null);
+
     const handleOpenWindow = () => {
-        window.open("", "_blank", "width=600,height=800");
+        window.open("", "_blank", "width=200,height=300");
+        function Babar(){
+            return(
+                <div>jgyufyt</div>
+            )
+        }
+    }
+    const handleCloseWindow = () => {
+        if (newWindow) {
+            newWindow.close(); 
+            setNewWindow(null); 
+        }
     };
 
     return (
@@ -15,6 +29,8 @@ function App() {
             {/* <Machine setData={setCartCount}/> */}
             {/* <Login/> */}
             <button onClick={handleOpenWindow}>Open New Window</button>
+            <button onClick={handleCloseWindow}>Open New Window</button>
+
         </div>
     );
 }
