4571615539ed90a3d924e2942dddd6e8d238a3a5
SYED SHABI UL HASSAN <tanzeel0680@gmail.com>
2025-05-23 11:53:56 +0500

    fyfy

--- src/Login/Next.js
+++ src/Login/Next.js
@@ -2,125 +2,50 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function Next(){
-const [isLogin, setIsLogin] = useState(true);
-const [loggedInUser, setLoggedInUser] = useState(null);
+  const [isLogin, setIsLogin] = useState(true);
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
-const {
+  const {
     register,
     handleSubmit,
     reset,
     formState: {errors}
-} = useForm();
+  } = useForm();
+  const getUsers = () => JSON.parse(localStorage.getItem("users")) || [];
 
-const getUser = () => JSON.parse(localStorage.getItem("user")) || [];
-
-const onSubmit = (data) => {
-    let users = getUser();
+  const onSubmit = () => {
+    let users = getUsers();
 
     if(isLogin){
-    const foundUser = users.find(
-        (user) => user.email === data.email && user.password === data.password
-      );
+    const foundUser =  users.find(
+    (user) => user.email === data.email && user.password === data.password
+    );
     if(foundUser){
-        setLoggedInUser(foundUser.name)
-        alert(`wellcome ${foundUser.name}`);
+      setLoggedInUser(foundUser.name);
+      alert(`wellCome ${foundUser.name}`);
     }else{
-        alert("Not Current");
+      alert("Not Currentt");
     }
     }else{
     const emailExit = users.some((user) => user.email === data.email);
     if(emailExit){
-    alert("AlReady Hai");
+      alert("Already hai");
     }else{
-        const newUser = {
-        name: data.name,
-        email: data.email,
-        password: data.password
-        };
+    const newUser = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
     const updateUser = [... users, newUser];
-    localStorage.setItem("users", JSON.stringify(updateUser));
-    alert("sign succes");
-    setIsLogin("");
+    localStorage.setItem("users",JSON.stringify(updateUser));
+    setIsLogin(true);
+    alert("sign success");
     reset();
-}
-}
-};
-
-
-    return(
-        <>
-<div id="main">
-      <h2>{isLogin ? "Login" : "Signup"}</h2>
-      {loggedInUser ? (
-        <h3>Hello {loggedInUser}</h3>
-      ) : (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {!isLogin && (
-            <div className="name">
-              <label>Name:</label>
-              <input
-                type="text"
-                placeholder="Name"
-                {...register("name", {
-                  maxLength: 3, 
-                  required: !isLogin && "Name is required",
-                  message: "3length"
-                  
-                })}
-              />
-              {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
-            </div>
-          )}
-
-          <div className="email">
-            <label>Email:</label>
-            <input
-              type="email"
-              placeholder="Email"
-              {...register("email", {
-                required: "Email is required",
-                pattern: {
-                  value: /^\S+@\S+\.\S+$/,
-                  message: "Invalid email format"
-                }
-              })}
-            />
-            {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
-          </div>
-
-          <div className="password">
-            <label>Password:</label>
-            <input
-              type="password"
-              placeholder="Password"
-              {...register("password", {
-                required: "Password is required",
-                minLength: {
-                  value: 6,
-                  message: "At least 4 characters"
-                }
-              })}
-            />
-            {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
-          </div>
-
-          <button className="submit" type="submit">
-            {isLogin ? "Login" : "Signup"}
-          </button>
-        </form>
-      )}
-
-      {!loggedInUser && (
-        <p>
-          {isLogin ? "Mere pass Account nhi hai?" : "Mere pass Account hai?"}
-          <button className="set" onClick={() => setIsLogin(!isLogin)}>
-            {isLogin ? "Signup" : "Login"}
-          </button>
-        </p>
-      )}
-    </div>
-
-        </>
-    )
+    }
+    }
+  };
+  return(
+    <></>
+  )
 }
 export default Next;
